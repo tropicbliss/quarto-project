7c347e670196516fc9229130ab77adbecb6d0994
tropicbliss <tropicbliss@protonmail.com>
2024-09-15 01:41:18 +0800

    Take home ex 1

---
+++ Take-home_Ex/Take-home_Ex01/js/unif_plot.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#unif_plot').scianimator({
+	    'images': ['unif_dir/unif_plot1.png', 'unif_dir/unif_plot2.png', 'unif_dir/unif_plot3.png', 'unif_dir/unif_plot4.png', 'unif_dir/unif_plot5.png', 'unif_dir/unif_plot6.png', 'unif_dir/unif_plot7.png', 'unif_dir/unif_plot8.png', 'unif_dir/unif_plot9.png', 'unif_dir/unif_plot10.png', 'unif_dir/unif_plot11.png', 'unif_dir/unif_plot12.png', 'unif_dir/unif_plot13.png', 'unif_dir/unif_plot14.png', 'unif_dir/unif_plot15.png', 'unif_dir/unif_plot16.png', 'unif_dir/unif_plot17.png', 'unif_dir/unif_plot18.png', 'unif_dir/unif_plot19.png', 'unif_dir/unif_plot20.png', 'unif_dir/unif_plot21.png', 'unif_dir/unif_plot22.png', 'unif_dir/unif_plot23.png', 'unif_dir/unif_plot24.png', 'unif_dir/unif_plot25.png', 'unif_dir/unif_plot26.png', 'unif_dir/unif_plot27.png', 'unif_dir/unif_plot28.png', 'unif_dir/unif_plot29.png', 'unif_dir/unif_plot30.png', 'unif_dir/unif_plot31.png', 'unif_dir/unif_plot32.png', 'unif_dir/unif_plot33.png', 'unif_dir/unif_plot34.png', 'unif_dir/unif_plot35.png', 'unif_dir/unif_plot36.png', 'unif_dir/unif_plot37.png', 'unif_dir/unif_plot38.png', 'unif_dir/unif_plot39.png', 'unif_dir/unif_plot40.png', 'unif_dir/unif_plot41.png', 'unif_dir/unif_plot42.png', 'unif_dir/unif_plot43.png', 'unif_dir/unif_plot44.png', 'unif_dir/unif_plot45.png', 'unif_dir/unif_plot46.png', 'unif_dir/unif_plot47.png', 'unif_dir/unif_plot48.png', 'unif_dir/unif_plot49.png', 'unif_dir/unif_plot50.png', 'unif_dir/unif_plot51.png', 'unif_dir/unif_plot52.png'],
+	    'width': 480,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#unif_plot').scianimator('play');
+    });
+})(jQuery);
